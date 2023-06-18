9e1ece6bedf27ffe698c08847564babe31d5bafe
Jeff Kumar <jeffkumar.aw@gmail.com>
2023-06-18 18:59:53 -0400

    more updates

--- src/components/Launch/CreateTrip.js
+++ src/components/Launch/CreateTrip.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, ButtonGroup, IconButton, Typography } from '@mui/material'; 
+import CarRentalIcon from '@mui/icons-material/CarRental';
+import FlightIcon from '@mui/icons-material/Flight'; 
+import ShareIcon from '@mui/icons-material/Share';  
 
 const columns = [ 
   { field: 'description', headerName: 'Description', width: "100%" }, 
@@ -19,7 +22,7 @@ export default function CreateTrip({date, duration, location, itineraryItems}) {
     <div style={{ width: '100%', marginBottom: "20px"}}>
         <Box style={{marginBottom: "20px"}}> 
             <Typography variant="h6" component="h1" gutterBottom>
-                Your Trip
+                Your Trip details
             </Typography>  
             <div>
                 Trip Date: {date}
@@ -30,20 +33,48 @@ export default function CreateTrip({date, duration, location, itineraryItems}) {
             <div>
                 Location: {location?.description}
             </div>
+
+            <div style={{ width: '100%', margin: "10px 0 10px", display: "flex", flexDirection: "row", justifyContent:"flex-start"}}>
+                <div>
+                    Add Flight: 
+                    <IconButton color="primary" aria-label="share">
+                        <FlightIcon />
+                    </IconButton>
+                </div>
+                <div>
+                    Add Car: 
+                    <IconButton color="primary" aria-label="share">
+                        <CarRentalIcon />
+                    </IconButton>
+                </div>
+            </div>
+            
+           
         </Box>
-        
-        
-        <DataGrid
-            rows={rows}
-            columns={columns}
-            initialState={{
-            pagination: {
-                paginationModel: { page: 0, pageSize: 5 },
-            },
-            }}
-            pageSizeOptions={[5, 10]}
-            checkboxSelection
-        />
+        <Box>
+            
+            <div style={{ width: '100%', margin: "10px 0 10px", display: "flex", flexDirection: "row", justifyContent:"space-between"}}>
+                <Button variant="outlined">Add Itinerary Item</Button>
+                <IconButton color="primary" aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+            </div>
+            
+            {rows?.length > 1 ? 
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                    },
+                    }}
+                    pageSizeOptions={[5, 10]}
+                    checkboxSelection
+                />
+                : null
+            }
+        </Box>  
     </div>
   );
 }
